a54495e992e97d9b6488241741c70328dae6d485
oblivioussolutions <oblivious.solutions@gmail.com>
2013-08-14 21:51:31 -0500

    Update Player.java
    Added methods to player

--- Player.java
+++ Player.java
@@ -1,42 +1,146 @@
-import java.util.HashSet;
+package oblivioussolutions.game;
+
+import java.awt.Color;
+import java.util.ArrayList;
 
 public class Player {
-	private String name;
-	private boolean male;
-	private boolean madeFalseAccusation;
-	private BoardSpace position;
-	private HashSet<Card> cards;
+	
+	private int suspectNumber = 0;
+	private String suspectName = null;
+	private String playerName = "";
+	private Card playerCard = null;
+	private ArrayList<Card> cardsInHand = new ArrayList<Card>();
+	private boolean computerPlayer = false;
+	private Location playerLocation = null;
+	private Notebook notebook = null;
+	private Color playerColor = Color.gray;
+	private boolean hasMadeFalseAccusation = false;
+	
+	public static final Color COLOR_SCARLET = Color.red;
+	public static final Color COLOR_GREEN = Color.green;
+	public static final Color COLOR_MUSTARD = Color.yellow;
+	public static final Color COLOR_PLUM = Color.magenta;
+	public static final Color COLOR_WHITE = Color.black;
+	public static final Color COLOR_PEACOCK = Color.blue;
 
-	public Player(String aName, boolean aMale, BoardSpace aPosition) {
-		this.name = aName;
-		this.male = aMale;
-		this.position = aPosition;
-		this.madeFalseAccusation = false;
-	}
-
-	public boolean isMale() {
-		return this.male;
-	}
-
-	public BoardSpace getPosition() {
-		return this.position;
-	}
-
-	public String getName() {
-		return this.name;
+	public Player() {
+		
 	}
 	
-	public boolean madeFalseAccusation(){
-		return this.madeFalseAccusation;
+	public Player(Card pick, String name, Color color, boolean computer) {
+		setPlayerName(name);
+		setPlayerCard(pick);
+		setSuspectNumber(pick.getValue());
+		setSuspectName(getPlayerCard().toString());
+		setComputerPlayer(computer);
+		setPlayerColor(color);
+	}
+	
+	public void setLocation(Location location) {
+		playerLocation = location;
+	}
+	
+	public Location getLocation() {
+		return playerLocation;
+	}
+		
+	public void setDetectiveNotebook(DetectiveNotebook notebook) {
+		this.notebook = notebook;
+	}
+	
+	public DetectiveNotebook getDetectiveNotebook() {
+		return notebook;
+	}
+	
+	public void addCard(Card card) {
+		cardsInHand.add(card);
+	}
+	
+	public ArrayList<Card> getCardsInHand() {
+		return cardsInHand;
+	}
+	
+	public boolean isCardInHand(Card card) {
+		return cardsInHand.contains(card);
+	}
+	
+	public boolean isCardInHand(int type, int id) {
+		Card card = new Card(type, id);
+		return cardsInHand.contains(card);
+	}
+	
+	public boolean isHoldingCardInSuggestion(ArrayList<Card> suggestion) {
+		boolean hasCards = false;
+		for (Card card : cardsInHand) {
+			if (suggestion.contains(card)) hasCards = true;
+		}
+		return hasCards;
+	}
+	
+	
+	public String toString() {
+		return getPlayerCard().toString();
+	}
+	
+	public String toLongString() {
+		String location = (playerLocation != null && playerLocation.getRoomId() != -1? "in the " + playerLocation.getRoomCard().toString():"outside of a room");
+		return getPlayerCard().toString() + ", played by " + (isComputerPlayer()?"computer":getPlayerName()) + " is currently " + location + ".";
 	}
 
-	public void move(BoardSpace aPosition) {
+	public String getSuspectName() {
+		return suspectName;
 	}
 
-	public void assignCard(Card aCard) {
+	public void setSuspectName(String suspectName) {
+		this.suspectName = suspectName;
 	}
 
-	public boolean hasCard(Card aCard) {
-		return this.cards.contains(aCard);
+	public Card getPlayerCard() {
+		return playerCard;
 	}
+
+	public void setPlayerCard(Card playerCard) {
+		this.playerCard = playerCard;
+	}
+
+	public Color getPlayerColor() {
+		return playerColor;
+	}
+
+	public void setPlayerColor(Color playerColor) {
+		this.playerColor = playerColor;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public int getSuspectNumber() {
+		return suspectNumber;
+	}
+
+	public void setSuspectNumber(int suspectNumber) {
+		this.suspectNumber = suspectNumber;
+	}
+
+	public boolean isComputerPlayer() {
+		return computerPlayer;
+	}
+
+	public void setComputerPlayer(boolean computerPlayer) {
+		this.computerPlayer = computerPlayer;
+	}
+	
+	public boolean hasMadeFalseAccusation() {
+		return hasMadeFalseAccusation;
+	}
+	
+	public void setHasMadeFalseAccusation() {
+		hasMadeFalseAccusation = true;
+	}
+
 }
